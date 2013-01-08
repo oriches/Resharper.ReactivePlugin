5414f76e800cfcbd3dd08f14e691daffeac43a93
Ollie Riches <ollie.riches@btinternet.com>
2013-01-08 22:52:34 +0000

    Added more detail to test

---
+++ Resharper.ReactivePlugin/Resharper.ReactivePlugin.Tests.Classes/CustomExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Resharper.ReactivePlugin.Tests.Classes
+{
+    public static class CustomExtensions
+    {
+        public static int Add(this int number, int number2)
+        {
+            return number + number2;
+        }
+    }
+}
